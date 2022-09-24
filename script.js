6e7509db553a720ae92b0e67f23e2f424066cf46
hammzyy <okenlaha@gmail.com>
2022-09-24 23:44:05 +0100

    js start

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-document.querySelector
+const nextonly = document.querySelector("#nextonly");
+const next = document.querySelector(".next")
+const bTF = document.querySelector("#backToForm")
+const bTPI = document.querySelector("#backToPi")
+const submit = document.querySelector("#submit")
+
+function inNext(){
+    
+}
+
+function toNext(){
+
+}
+
+function backTF(){
+
+}
+
+function backTPI(){
+
+}
+
+function done(){
+    
+}
